18c625bac8a3fb75ffc15dc44e51c8c8272a907a
ebruaeo <ebruaeo@hotmail.com>
2024-12-27 14:59:41 +0300

    updating UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+
 }
--- app/src/main/java/com/example/mvvm_cities/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_cities/view/MainActivity.kt
@@ -2,20 +2,43 @@ package com.example.mvvm_cities.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
 import com.example.mvvm_cities.R
+import com.example.mvvm_cities.databinding.ActivityMainBinding
+import com.example.mvvm_cities.viewmodel.CityViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val model: CityViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        model.getCityData().observe(this, Observer { city ->
+            binding.cityImage.setImageDrawable(
+                ResourcesCompat.getDrawable(resources, city.image, applicationContext.theme)
+            )
+            binding.cityNameTV.text = city.name
+            binding.cityPopulationTV.text = city.population.toString()
+        })
     }
 }
